83db95e07fa527d53ef8739a0df2677bf2a3057b
Xiaoqi Cao <xqcao@xqcao.com>
2023-02-25 00:05:21 +0800

    add string convert to int funcs

--- stringutil/stringutil.go
+++ stringutil/stringutil.go
@@ -1,6 +1,8 @@
 package stringutil
 
 import (
+	"log"
+	"strconv"
 	"strings"
 )
 
@@ -18,4 +20,28 @@ func IsBlank(str string) bool {
 
 func IsNotBlank(str string) bool {
 	return !IsBlank(str)
+}
+
+func ToInt(str string) int {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		log.Panicf("String: %s convert to int failure.", str)
+	}
+	return i
+}
+
+func ToInt64(str string) int64 {
+	i, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		log.Panicf("String: %s convert to int64 failure.", str)
+	}
+	return i
+}
+
+func ToUInt64(str string) uint64 {
+	i, err := strconv.ParseUint(str, 10, 64)
+	if err != nil {
+		log.Panicf("String: %s convert to uint64 failure.", str)
+	}
+	return i
 }
